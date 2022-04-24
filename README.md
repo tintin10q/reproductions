501eabcb2ba38a0ab441881715df4fd28b1d7fff
Tintin10q <tintin10q@hotmail.com>
2022-04-24 22:14:39 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Pinia bug report
+
+This is a bug report start to report a bug with Pinia. It's configured to use Vite and Vue 3. You can edit and fork [on StackBlitz](http://stackblitz.com/github/piniajs/bug-report) or just [using this repository as a template](https://github.com/piniajs/bug-report/generate).
---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useCounter, } from "./stores/counter";
+
+const counter = useCounter()
+</script>
+
+<template>
+  <p>The counter is at {{ counter.n }}</p>
+  <button @click="counter.n++">Increment</button>
+  <button @click="counter.n--">Decrement</button>
+  <button @click="counter.n = 0">Reset</button>
+</template>
